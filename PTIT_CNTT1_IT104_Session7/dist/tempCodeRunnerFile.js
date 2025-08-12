107fa026b691f865d3563fe8b8e5139e8337f24b
duongtram1702 <duong170226@gmail.com>
2025-08-12 23:02:08 +0700

    submit

--- PTIT_CNTT1_IT104_Session7/dist/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-constructor(accountNumber, string, balance, number, history, HistoryTransaction[], status, string, public, interestRate, number);
-{
-    super(accountNumber, balance, history, status);
-}
