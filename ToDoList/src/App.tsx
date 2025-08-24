f4ccad0775ead3d1278f34e414598758ed55a0bb
duongtram1702 <duong170226@gmail.com>
2025-08-24 21:39:07 +0700

    submit

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -1,9 +1,14 @@
-import ToDoList from './prop_state_event/components/todolist/ToDoList';
+import BaseButton from './components/base/button';
+// import ToDoList from './prop_state_event/components/todolist/ToDoList';
 
 function App() {
     return (
         <>
-            <ToDoList />
+            <BaseButton htmlType="button" size="small" />
+            <BaseButton htmlType="button" size="medium" />
+            <BaseButton htmlType="button" />
+            <BaseButton htmlType="button" />
+            <BaseButton htmlType="button" />
         </>
     );
 }
