81a5db78e5293b93785265e76fd3b1606d34b3aa
duongtram1702 <duong170226@gmail.com>
2025-09-16 00:24:04 +0700

    submit

---
+++ PTIT_CNTT1_IT104_Session29/client/src/components/Bai2.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
+interface Product {
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+    quantity: number;
+    created_at: string;
+}
+
+export const Bai2 = () => {
+    const getAllProduct = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/product`);
+
+            // ✅ Lấy đúng đường dẫn
+            const products: Product[] = response.data || [];
+
+            console.log('Products:', products);
+        } catch (error) {
+            console.error('Lỗi khi load dữ liệu:', error);
+        }
+    };
+
+    useEffect(() => {
+        getAllProduct();
+    }, []);
+
+    return <></>;
+};
